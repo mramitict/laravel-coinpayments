ef5226bada89494b74dbdcc1705b2e48c1e6cc7f
Amit Kumar <mramitict@users.noreply.github.com>
2018-02-23 18:43:30 +0530

    Added new commands
    Commands has been added

--- src/Kevupton/LaravelCoinpayments/CoinpaymentsCommands.php
+++ src/Kevupton/LaravelCoinpayments/CoinpaymentsCommands.php
@@ -10,4 +10,11 @@ abstract class CoinpaymentsCommands {
     const GET_CALLBACK_ADDRESS = 'get_callback_address';
     const BALANCES = 'balances';
     const RATES = 'rates';
+    const GET_BASIC_INFO = 'get_basic_info';
+    const GET_DEPOSIT_ADDRESS = 'get_deposit_address';
+    const GET_TRANSACTION_INFO = 'get_tx_info_multi';
+    const GET_TRANSACTION_LIST = 'get_tx_ids';
+    const CONVERT = 'convert';
+    const GET_WITHDRAWL_HISTORY = 'get_withdrawal_history';
+    const GET_CONVERSION_INFO = 'get_conversion_info';
 }
